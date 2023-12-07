d4fbc8ccc6581ad411f631c03bbfef29e8da4def
bd1026 <4b0g0028@stust.edu.tw>
2023-12-07 16:54:59 +0800

    載入影像並灰階化顯示

---
+++ 載入影像並灰階化顯示/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.載入影像檔 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.影像灰階化 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // 載入影像檔
+            // 
+            this.載入影像檔.Location = new System.Drawing.Point(132, 315);
+            this.載入影像檔.Name = "載入影像檔";
+            this.載入影像檔.Size = new System.Drawing.Size(107, 41);
+            this.載入影像檔.TabIndex = 0;
+            this.載入影像檔.Text = "載入影像檔";
+            this.載入影像檔.UseVisualStyleBackColor = true;
+            this.載入影像檔.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 225);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // 影像灰階化
+            // 
+            this.影像灰階化.Location = new System.Drawing.Point(549, 315);
+            this.影像灰階化.Name = "影像灰階化";
+            this.影像灰階化.Size = new System.Drawing.Size(107, 41);
+            this.影像灰階化.TabIndex = 2;
+            this.影像灰階化.Text = "影像灰階化";
+            this.影像灰階化.UseVisualStyleBackColor = true;
+            this.影像灰階化.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Location = new System.Drawing.Point(451, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(346, 225);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.影像灰階化);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.載入影像檔);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button 載入影像檔;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button 影像灰階化;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ 載入影像並灰階化顯示/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "圖像文件(JPeg,Gif,Bmp,etc.)|*.jpg;*.jpeg;*.gif;*.bmp;*tif;*tiff;*.png|所有文件(*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap MyBitmap = new Bitmap(openFileDialog.FileName);
+                    this.pictureBox1.Image = MyBitmap;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"訊息顯示");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for(int x=0;x<Width; x++)
+                    for(int y = 0; y < Height; y++)
+                    {
+                        pixel = oldBitmap.GetPixel(x, y);
+                        int r, g, b, Result = 0;
+                        r = pixel.R;
+                        g= pixel.G;
+                        b = pixel.B;
+                        Result = (299 * r + 587 * g + 114 * b) / 1000;
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                    }
+                this.pictureBox2.Image = newBitmap;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息顯示");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
